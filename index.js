eeec711ae8a7cb79de3fecb0cd2e4a047e2f0bb6
cerveux <diegomartinguzman@hotmail.com.ar>
2023-04-15 00:14:34 -0300

    "implementado el fetch al backend"

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import Card from "./Components/Card.js"
-import teammates from "./team.js"
 
+let teammates;
 
+const apiUrl = "https://ap-backend-production-6742.up.railway.app"
+
+await fetch(`${apiUrl}/usuarios`)
+  .then(response => response.json())
+  .then(data => teammates = data.teammates)
+  
 
 const app = `<div class="container d-flex  justify-content-center flex-wrap">
 ${teammates.map((alumno)=>{
@@ -11,5 +17,7 @@ ${teammates.map((alumno)=>{
 }).join("")}
 </div>`
 
-console.log("holo");
+
 $(".root-team").html(app)
+
+
