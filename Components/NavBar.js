5f0e2ceaae4510dc07b989b8a5d1ee84ce7de3f7
cerveux <diegomartinguzman@hotmail.com.ar>
2023-04-16 20:33:05 -0300

    "prueba"

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -16,28 +16,28 @@ const NavBar = () => {
         <div class="collapse navbar-collapse ms-auto" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="/index.html">
+                    <a class="nav-link" aria-current="page" href="index.html">
                         <font style="vertical-align: inherit;">
                             <font style="vertical-align: inherit; color: #F6F1F1 ;font-family: 'Roboto', sans-serif;">Inicio</font>
                         </font>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/team.html">
+                    <a class="nav-link" href="team.html">
                         <font style="vertical-align: inherit;">
                             <font style="vertical-align: inherit; color: #F6F1F1 ;font-family: 'Roboto', sans-serif;">Nosotros</font>
                         </font>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/form.html">
+                    <a class="nav-link" href="form.html">
                         <font style="vertical-align: inherit;">
                             <font style="vertical-align: inherit; color: #F6F1F1 ;font-family: 'Roboto', sans-serif;">Servicio</font>
                         </font>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/contact.html">
+                    <a class="nav-link" href="contact.html">
                         <font style="vertical-align: inherit;">
                             <font style="vertical-align: inherit; color: #F6F1F1 ;font-family: 'Roboto', sans-serif;">Contacto</font>
                         </font>
