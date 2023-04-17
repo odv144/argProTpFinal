34920e871bca422fcfb6657ef20d5eb0b0c78a87
cerveux <diegomartinguzman@hotmail.com.ar>
2023-04-16 22:11:10 -0300

    "rutas"

--- Components/NavBar.js
+++ Components/NavBar.js
@@ -2,10 +2,10 @@ const NavBar = () => {
   return (
     `<nav class="navbar navbar-expand-lg shadow" style="background-color: #000000 !important;">
     <div class=" navbar container mx-2 mx-sm-auto" data-bs-theme="dark">
-        <a class="navbar-brand d-none d-sm-block" href="./index.html">
+        <a class="navbar-brand d-none d-sm-block" href="/">
             <img src="assets/logo.png" alt="" style="width: 250px;">
         </a>
-        <a class="navbar-brand d-block d-sm-none" href="./index.html">
+        <a class="navbar-brand d-block d-sm-none" href="/">
             <img src="assets/small-logo.png" alt="" style="width: 50px;">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -16,7 +16,7 @@ const NavBar = () => {
         <div class="collapse navbar-collapse ms-auto" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" aria-current="page" href="index.html">
+                    <a class="nav-link" aria-current="page" href="/">
                         <font style="vertical-align: inherit;">
                             <font style="vertical-align: inherit; color: #F6F1F1 ;font-family: 'Roboto', sans-serif;">Inicio</font>
                         </font>
